0e673ab0eb5e7ac014ca80ab50c05f26c879c091
ThanhGT <tran67@sheridancollege.ca>
2020-06-22 00:19:46 -0400

    Add project files.

---
+++ Calculatorv2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.ComponentModel.Design;
+using System.Xml;
+
+/// <summary>
+/// Created By: Gaurav Tayade, 
+///             Thanh  Tran,
+///             Robin Lansiquot,
+///             Bilaal Rashid
+/// Description: This console application takes two numbes as input from user
+///              and performs opearation on them wanted by user.
+/// Inputs: n1 and n2.
+/// Output: result gerated fron arithematic operation performed on numbers n1 and n2.
+/// </summary>
+
+namespace Calculator
+{
+    class Calculatorv2
+    {
+
+        static void Main(string[] args)
+        {
+            double n1 = 0;
+            double n2 = 0;
+
+            //flag to determine if the program should run or not
+            bool isRunning = true;
+
+            while (isRunning) {
+
+                // instantiate an instance object from the result class
+                Result result = new Result();
+
+                Console.Write("Enter an Integer: ");
+                if (double.TryParse(Console.ReadLine(), out n1)) 
+                {
+                    Console.Write("Enter a second integer: ");
+                    if (double.TryParse(Console.ReadLine(), out n2))
+                    {
+                        Console.Write("Enter an operator or QUIT (+, -, *, /, %): ");
+                        // read user input on operator
+                        string op = Console.ReadLine();
+
+                        // call the result instance's setResult method 
+                        result.setResult(n1, n2, op);
+                        Console.Write($"{n1} {op} {n2} = {result.getResult()}\n");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not a Number! Try Again!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not a Number! Try again!");
+                }
+                
+                    
+            } // end of while loop
+
+            //terminal will sleep in 9secs
+            Thread.Sleep(9000);
+            // terminate system
+            System.Environment.Exit(0);
+        } // end of main
+    }   // end of class
+}  // end of namespace
---
+++ Result.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Result
+    {
+        // field for doubleResult
+        private double doubleResult;
+
+        // determines computation depending on operation
+        public void setResult(double x, double y, string op)
+        {
+            if (op == "+")
+            {
+                doubleResult = x + y;
+            }
+            else if (op == "-")
+            {
+                doubleResult = x - y;
+            }
+            else if (op == "*")
+            {
+                doubleResult = x * y;
+            }
+            else if (op == "/")
+            {
+                // check if two numbers are divisable
+                if (y != 0)
+                    doubleResult = x / y;
+                else
+                    Console.Write($"ERROR: {x} and {y} is not divisable! ");
+
+            }
+            else if (op == "%")
+            {
+                // check if two numbers are divisable
+                if (y != 0)
+                    doubleResult = x % y;
+                else
+                    Console.Write($"ERROR: {x} and {y} is not divisable! ");
+            }
+            else if (op.ToUpper() == "QUIT")
+            {
+                System.Environment.Exit(0);
+            }
+            else
+            {
+                // in the case that the user enters something else other than the recommended inputs
+                Console.Write("Wrong input! Please try again.");
+            }
+        }
+
+        // getter for doubleResult
+        public double getResult()
+        {
+            return doubleResult;
+        }
+    }
+}
+
